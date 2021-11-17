e6ba10ef23a1d43f579d82326cf8c0b91f8326e8
elizaveta199 <93157218+elizaveta199@users.noreply.github.com>
2021-11-17 10:48:48 +0300

    make better

--- src/test/java/serviceGame/GameTest.java
+++ src/test/java/serviceGame/GameTest.java
@@ -4,52 +4,85 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
-    boolean isGreenLight = false;
+
     @Test
-    public void winnersNames() {
+    public void winnersNamesCase1() {
         String[] players = {"Kolya 7", "Dasha 9", "Ivan 3"};
-        String[] actual = Game.winnersNames(players);
-        if (!isGreenLight) {
-            String[] expected = {"Kolya", "Ivan"};
-            Assertions.assertArrayEquals(expected, actual);
-        } else {
-            String[] expected = {"Kolya", "Dasha", "Ivan"};
-            Assertions.assertArrayEquals(expected, actual);
-        }
+
+        String[] actual = Game.winnersNames(players, false, 7);
+        String[] expected = {"Kolya", "Ivan"};
+
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldFindLose() {
+    public void winnersNamesCase2() {
+        String[] players = {"Kolya 7", "Dasha 9", "Ivan 3"};
 
-        int[] arr = {5, 0, 9, 7, 0, 6};
-        int actual = Game.firstMethod(arr);
-        int expected;
-        if (!isGreenLight) {
-            expected = 1;
-        } else {
-            expected = 0;
-        }
-        Assertions.assertEquals(expected, actual, "MAX_SPEED=7");
+        String[] actual = Game.winnersNames(players, true, 5);
+        String[] expected = {"Kolya", "Dasha", "Ivan"};
+
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldFindSpeedLoseIfNoGreenLight() {
+    public void shouldFindLoseCase1() {
         int[] arr = {5, 0, 9, 7, 0, 6};
-        int[] actual = Game.secondMethod(arr);
-        int[] expected = {9};
-        Assertions.assertArrayEquals(expected, actual, "Если isGreenLight=false, MAX_SPEED=7");
+
+        int actual = Game.getCountOutOfTheGame(arr, false, 4);
+        int expected = 4;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLoseCase2() {
+        int[] arr = {5, 0, 9, 7, 0, 6};
+
+        int actual = Game.getCountOutOfTheGame(arr, true, 6);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindSpeedLoseCase1() {
+        int[] arr = {5, 0, 9, 7, 0, 6};
+
+        int[] actual = Game.getSpeedOutOfTheGame(arr, false, 2);
+        int[] expected = {5, 9, 7, 6};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindSpeedLoseCase2() {
+        int[] arr = {5, 0, 9, 7, 0, 6};
+
+        int[] actual = Game.getSpeedOutOfTheGame(arr, true, 6);
+        int[] expected = {};
+
+        Assertions.assertArrayEquals(expected, actual);
     }
 
 
     @Test
-    public void shouldFindSpeedWinIfNoGreenLight() {
+    public void shouldFindSpeedWinCase1() {
         int[] arr = {5, 0, 9, 7, 0, 6};
-        int[] actual = Game.thirdMethod(arr);
-        int[] expected = {5, 0, 7, 0, 6};
-        Assertions.assertArrayEquals(expected, actual, "Если isGreenLight=false, MAX_SPEED=7");
+
+        int[] actual = Game.getSpeedSurvivors(arr, false, 5);
+        int[] expected = {5, 0, 0};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindSpeedWinCase2() {
+        int[] arr = {5, 0, 9, 7, 0, 6};
+
+        int[] actual = Game.getSpeedSurvivors(arr, true, 0);
+        int[] expected = {5, 0, 9, 7, 0, 6};
+
+        Assertions.assertArrayEquals(expected, actual);
     }
 }
-
-
-
-
